e4d9977727c1e5e6f3eed178824ff8cb64d1b724
Ricky Mormor <rickymormor1@gmail.com>
2022-02-09 22:48:20 +0000

    Make code clean

--- src/components/detailsPage/CountryD.js
+++ src/components/detailsPage/CountryD.js
@@ -1,9 +1,6 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
-
 
 import './CountryD.css';
 import plainBGpreview from '../../images/plainBGpreview.png';
--- src/components/homepage/Country.js
+++ src/components/homepage/Country.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
 
@@ -15,33 +13,20 @@ import { fetchCountries } from '../../redux/store/countries';
 import { useSelector } from 'react-redux';
 import { getSelectedCountrySuccess } from '../../redux/store/countries';
 
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
-
 const Countries = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   
   useEffect(() => {
-    console.log('state before', countries);
     dispatch(fetchCountries());
-    console.log('state after', countries);
     
   }, [dispatch]);
   const countries = useSelector(state => state.countries.countries.singleCountry);
-  console.log('state after outside', countries);
-  console.log('state before outside', countries);
 
   function selectedCountryName (country) {
-    console.log('country', country);
     dispatch(getSelectedCountrySuccess(country));
     navigate('/country');
   }
-
 
   return (
     <Box sx={{ width: '100%' }}>
@@ -66,79 +51,5 @@ const Countries = () => {
     </Box>
     );
 };
-
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
-    author: '@bkristastucchio',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-    author: '@rollelflex_graphy726',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-    author: '@helloimnik',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-    author: '@nolanissac',
-    cols: 2,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-    author: '@hjrc33',
-    cols: 2,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-    author: '@arwinneil',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-    author: '@tjdragotta',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-    author: '@katie_wasserman',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-    author: '@silverdalex',
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-    author: '@shelleypauls',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-    author: '@peterlaster',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-    author: '@southside_customs',
-    cols: 2,
-  },
-];
 
 export default Countries ;
